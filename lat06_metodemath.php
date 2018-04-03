1fdc897f8d898e97a3c51177c14215e0130d540e
sugengkrnwn <wawan.optimiz@gmail.com>
2018-04-03 17:48:27 +0700

    update

---
+++ lat06_metodemath.php
@@ -0,0 +1,15 @@
+<?php
+$a = 2.6;
+echo "Random ".rand(5,12);
+echo "<br>";
+echo "max".max(5,12);
+echo "<br>";
+echo "min".min(5,12);
+echo '<br>';
+echo "round".round($a);  
+
+/*
+metode math
+round , rand , max , min
+ */
+  ?>
---
+++ lat07_array.php
@@ -0,0 +1,10 @@
+<?php
+
+$nama = array('Harun','Wawan','Hasan');
+$hewan = ['kuda','kucing','aytam'];
+echo print_r($nama);
+echo "<br>";
+echo $nama[0];
+echo "<br>";
+echo $hewan[1];
+  ?>
