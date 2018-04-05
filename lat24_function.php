8dd91204f348fb2c9a89c0f1d1dba6fc05b5c750
sugengkrnwn <wawan.optimiz@gmail.com>
2018-04-05 20:03:46 +0700

    function pada php

---
+++ lat24_function.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h2>function pada php</h2>
+    <?php
+    function panggil(){ //deklarasi
+        echo "Kota kretek";
+    }
+    function jarak(){
+        echo '<br>';
+    }
+    panggil(); //memanggil function
+    jarak();
+    panggil();
+    jarak();
+    panggil();
+    ?>    
+</body>
+</html>
